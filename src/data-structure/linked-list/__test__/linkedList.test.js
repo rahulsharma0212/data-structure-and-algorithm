4a696f89001fb3921699bdce20cad28c54cb217a
Rahul Sharma <rahulsharma02121998@gmail.com>
2023-06-05 23:00:42 +0530

    push method added with toString,toArray utility

--- src/data-structure/linked-list/__test__/linkedList.test.js
+++ src/data-structure/linked-list/__test__/linkedList.test.js
@@ -4,5 +4,20 @@ describe("LinkedList", () => {
   it("should create empty linked list", () => {
     const linkedList = new LinkedList();
     expect(linkedList.isEmpty()).toBeTruthy();
+    expect(linkedList.size()).toBe(0);
+    expect(linkedList.toArray()).toEqual([]);
+    expect(linkedList.toString()).toEqual("");
+  });
+
+  it("should push data to linked list", () => {
+    const linkedList = new LinkedList();
+    linkedList.push(2);
+    linkedList.push(3);
+    expect(linkedList.length).toBe(2);
+    expect(linkedList.toArray()).toEqual([2, 3]); /*matching with order */
+    expect(linkedList.toArray()).toEqual(
+      expect.arrayContaining([3, 2])
+    ); /*matching with regardles of order */
+    expect(linkedList.toString()).toEqual("2,3");
   });
 });
--- src/data-structure/linked-list/linkedList.js
+++ src/data-structure/linked-list/linkedList.js
@@ -9,6 +9,37 @@ class LinkedList {
   isEmpty() {
     return this.length === 0;
   }
+
+  size() {
+    return this.length;
+  }
+
+  toArray() {
+    const array = [];
+    let current = this.head;
+    while (current) {
+      array.push(current.value);
+      current = current.next;
+    }
+    return array;
+  }
+
+  toString() {
+    return this.toArray().join();
+  }
+
+  push(value) {
+    const node = new LinkedListNode(value);
+    if (!this.tail) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return value;
+  }
 }
 
 export default LinkedList;
