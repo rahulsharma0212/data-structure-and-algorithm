d4798fd3d5ccf9a53bbfcdb4fd24ec006bda6c48
Rahul Sharma <rahulsharma02121998@gmail.com>
2023-06-06 01:27:56 +0530

    singly link list almost done

--- src/data-structure/linked-list/__test__/linkedList.test.js
+++ src/data-structure/linked-list/__test__/linkedList.test.js
@@ -9,10 +9,10 @@ describe("LinkedList", () => {
     expect(linkedList.toString()).toEqual("");
   });
 
-  it("PUSH || should push data to linked list", () => {
+  it("PUSH || should push data from the end of linked list", () => {
     const linkedList = new LinkedList();
-    linkedList.push(2);
-    linkedList.push(3);
+    expect(linkedList.push(2)).toBe(2);
+    expect(linkedList.push(3)).toBe(3);
     expect(linkedList.length).toBe(2);
     expect(linkedList.toArray()).toEqual([2, 3]); /*matching with order */
     expect(linkedList.toArray()).toEqual(
@@ -21,7 +21,19 @@ describe("LinkedList", () => {
     expect(linkedList.toString()).toEqual("2,3");
   });
 
-  it("POP || should pop data from linked list", () => {
+  it("UNSHIFT || should push data from start of linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.unshift(2)).toBe(2);
+    expect(linkedList.unshift(3)).toBe(3);
+    expect(linkedList.length).toBe(2);
+    expect(linkedList.toArray()).toEqual([3, 2]); /*matching with order */
+    expect(linkedList.toArray()).toEqual(
+      expect.arrayContaining([2, 3])
+    ); /*matching with regardles of order */
+    expect(linkedList.toString()).toEqual("3,2");
+  });
+
+  it("POP || should remove data from the end of linked list", () => {
     const linkedList = new LinkedList();
     expect(linkedList.pop()).toBeUndefined();
     expect(linkedList.push(1)).toBe(1);
@@ -33,5 +45,109 @@ describe("LinkedList", () => {
     expect(linkedList.toString()).toBe("");
     expect(linkedList.toArray()).toEqual([]);
     expect(linkedList.isEmpty()).toBeTruthy();
+  });
+
+  it("SHIFT || should remove data from start of linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.shift()).toBeUndefined();
+    expect(linkedList.push(1)).toBe(1);
+    expect(linkedList.push(2)).toBe(2);
+    expect(linkedList.shift()).toBe(1);
+    expect(linkedList.toString()).toBe("2");
+    expect(linkedList.toArray()).toEqual([2]);
+    expect(linkedList.shift()).toBe(2);
+    expect(linkedList.toString()).toBe("");
+    expect(linkedList.toArray()).toEqual([]);
+    expect(linkedList.isEmpty()).toBeTruthy();
+  });
+
+  it("GET || should get data at a particular index of linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.push(1)).toBe(1);
+    expect(linkedList.push(2)).toBe(2);
+
+    expect(linkedList.get(-1)).toBeUndefined();
+    expect(linkedList.get(5)).toBeUndefined();
+
+    expect(linkedList.get(0)).toBe(1);
+    expect(linkedList.get(1)).toBe(2);
+  });
+
+  it("SET || should get data at a particular index of linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.push(1)).toBe(1);
+    expect(linkedList.push(2)).toBe(2);
+
+    expect(linkedList.set(-1, 3)).toBeUndefined();
+    expect(linkedList.set(5, 4)).toBeUndefined();
+
+    expect(linkedList.set(0, 3)).toBe(3);
+    expect(linkedList.set(1, 4)).toBe(4);
+    expect(linkedList.toString()).toBe("3,4");
+  });
+
+  it("INSERT || should insert data at a particular index of linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.push(2)).toBe(2);
+    expect(linkedList.push(4)).toBe(4);
+
+    expect(linkedList.insert(-1, 3)).toBeUndefined();
+    expect(linkedList.insert(5, 4)).toBeUndefined();
+
+    expect(linkedList.insert(0, 1)).toBe(1);
+    expect(linkedList.insert(3, 5)).toBe(5);
+    expect(linkedList.insert(2, 3)).toBe(3);
+
+    expect(linkedList.toString()).toBe("1,2,3,4,5");
+  });
+
+  it("REMOVE || should remove data at a particular index of linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.push(1)).toBe(1);
+    expect(linkedList.push(2)).toBe(2);
+    expect(linkedList.push(3)).toBe(3);
+    expect(linkedList.push(4)).toBe(4);
+    expect(linkedList.push(5)).toBe(5);
+
+    expect(linkedList.remove(-1)).toBeUndefined();
+    expect(linkedList.remove(5)).toBeUndefined();
+
+    expect(linkedList.remove(0)).toBe(1);
+    expect(linkedList.remove(3)).toBe(5);
+    expect(linkedList.remove(1)).toBe(3);
+
+    expect(linkedList.toString()).toBe("2,4");
+  });
+
+  it("INDEXOF || should get index for particular value inside linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.push(1)).toBe(1);
+    expect(linkedList.push(2)).toBe(2);
+    expect(linkedList.push(3)).toBe(3);
+    expect(linkedList.push(4)).toBe(4);
+    expect(linkedList.push(5)).toBe(5);
+
+    expect(linkedList.indexOf(8)).toBe(-1);
+    expect(linkedList.indexOf(2)).toBe(1);
+    expect(linkedList.indexOf(4)).toBe(3);
+  });
+
+  it("INCLUDES || should give boolean(T|F) for particular value inside linked list", () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.push(1)).toBe(1);
+    expect(linkedList.push(2)).toBe(2);
+    expect(linkedList.push(3)).toBe(3);
+    expect(linkedList.push(4)).toBe(4);
+    expect(linkedList.push(5)).toBe(5);
+
+    expect(linkedList.includes(8)).toBeFalsy();
+    expect(linkedList.includes(2)).toBeTruthy();
+  });
+
+  it("FROMARRAY || should give boolean(T|F) for particular value inside linked list", () => {
+    const linkedList = new LinkedList();
+    const llFromArray = linkedList.fromArray([1, 2, 3, 4, 5]);
+    expect(llFromArray).toBeInstanceOf(LinkedList);
+    expect(llFromArray.toString()).toBe("1,2,3,4,5");
   });
 });
--- src/data-structure/linked-list/linkedList.js
+++ src/data-structure/linked-list/linkedList.js
@@ -62,6 +62,129 @@ class LinkedList {
       return current.value;
     }
   }
+  shift() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    const remove = this.head;
+    this.head = this.head.next;
+    if (this.head == null) {
+      this.tail = null;
+    }
+    this.length--;
+    return remove.value;
+  }
+
+  unshift(value) {
+    const node = new LinkedListNode(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      const temp = this.head;
+      this.head = node;
+      this.head.next = temp;
+    }
+    this.length++;
+    return value;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    let current = this.head;
+    let currentIndex = 0;
+    while (currentIndex < index) {
+      current = current.next;
+      currentIndex++;
+    }
+    return current.value;
+  }
+
+  set(index, value) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    let current = this.head;
+    let currentIndex = 0;
+    while (currentIndex < index) {
+      current = current.next;
+      currentIndex++;
+    }
+    current.value = value;
+    return value;
+  }
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      return undefined;
+    }
+
+    if (index === 0) {
+      return this.unshift(value);
+    } else if (index === this.length) {
+      return this.push(value);
+    } else {
+      let current = this.head;
+      let currentIndex = 0;
+      let previous = null;
+      while (currentIndex < index) {
+        previous = current;
+        current = current.next;
+        currentIndex++;
+      }
+      const node = new LinkedListNode(value, current);
+      previous.next = node;
+      this.length++;
+      return value;
+    }
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    if (index === 0) {
+      return this.shift();
+    } else if (index === this.length - 1) {
+      return this.pop();
+    } else {
+      let current = this.head;
+      let currentIndex = 0;
+      let prev = null;
+      while (currentIndex < index) {
+        prev = current;
+        current = current.next;
+        currentIndex++;
+      }
+      prev.next = current.next;
+      this.length--;
+      return current.value;
+    }
+  }
+  indexOf(value) {
+    let current = this.head;
+    let currentIndex = 0;
+    while (current != null) {
+      if (current.value === value) {
+        return currentIndex;
+      }
+      current = current.next;
+      currentIndex++;
+    }
+    return -1;
+  }
+
+  includes(value) {
+    return this.indexOf(value) !== -1;
+  }
+
+  fromArray(array) {
+    array.forEach((value) => {
+      this.push(value);
+    });
+    return this;
+  }
+  //TODO Reverse
 }
 
 export default LinkedList;
